eecf87c9f8139fd86fd98d8b50926cd57d71e3dd
Ksenx73 <hello@itcampe.com>
2021-10-14 16:46:54 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,14 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String tagline = ""; //напишите тут ваш код
+
+        System.out.println(tagline);
+    }
+}
